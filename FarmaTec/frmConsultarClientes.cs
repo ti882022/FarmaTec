6bab89183a104f8720bcf70864f26bb5ce8055ed
ti882022 <ti882022.projeto@gmail.com>
2022-10-07 22:17:54 -0300

    Criação das consultas com DataGrid

--- FarmaTec/frmConsultarClientes.cs
+++ FarmaTec/frmConsultarClientes.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TransferenciaDados;
 
 namespace FarmaTec
 {
@@ -17,6 +18,61 @@ namespace FarmaTec
             InitializeComponent();
         }
 
+
+        //Coleção para o AutoComplete
+        AutoCompleteStringCollection autocomplete = new AutoCompleteStringCollection();
+
+        public async void AutoCompletar()
+        {
+
+
+            try
+            {
+                //Instanciar as classes
+                ConsultarClientes consultarClientes = new ConsultarClientes();
+                ClientesDTO dados = new ClientesDTO();
+
+             //   txtnomeCliente.Text = "Ewerthon";
+
+                //Popular classe
+                dados.nome = txtnomeCliente.Text;
+               // dados.cpf = mskCpf.Text;
+
+                //Chamar o método
+                await consultarClientes.MostrarCliente(dados);
+
+                if (dados.mensagens == null)
+                {
+                    if (consultarClientes.listClientes.Count > 0)
+                    {
+                        //Percorrer a lista
+                        for (int i = 0; i < consultarClientes.listClientes.Count; i++)
+                        {
+                            autocomplete.Add(consultarClientes.listClientes[i].nomeCliente.ToString());
+                                                    }
+                        //Definir as propriedades do autocomplete do textbox
+                        txtnomeCliente.AutoCompleteMode = AutoCompleteMode.Suggest;
+                        txtnomeCliente.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                        txtnomeCliente.AutoCompleteCustomSource = autocomplete;
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(dados.mensagens);
+                }
+
+            }
+
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString());
+            }
+
+        }
+
+
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
@@ -24,10 +80,67 @@ namespace FarmaTec
 
         private void btnSair_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Deseja sair da Consulta?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            /* if (MessageBox.Show("Deseja sair da Consulta?", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+             {
+                 Close();
+             }
+
+             */
+
+            AutoCompletar();
+
+
+        }
+
+        private async void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+
+            try
             {
-                Close();
+                //Instanciar as classes
+                ConsultarClientes consultarClientes = new ConsultarClientes();
+                ClientesDTO dados = new ClientesDTO();
+
+                //Popular classe
+                dados.nome = txtnomeCliente.Text;
+
+                //Limpar fonte de dados e o DatagridView
+                dtClientes.DataSource = null;
+                dtClientes.Rows.Clear();
+
+                //Chamar o método
+                await consultarClientes.MostrarCliente(dados);
+
+                for (int i = 0; i < consultarClientes.listClientes.Count; i++)
+                {
+                    dtClientes.Rows.Add(consultarClientes.listClientes[i].codCliente.ToString(),
+                                            consultarClientes.listClientes[i].nomeCliente.ToString(),
+                                            consultarClientes.listClientes[i].endereco.ToString(),
+                                            consultarClientes.listClientes[i].cep.ToString(),
+                                            consultarClientes.listClientes[i].cidade.ToString(),
+                                            consultarClientes.listClientes[i].bairro.ToString(),
+                                            consultarClientes.listClientes[i].uf.ToString(),
+                                            consultarClientes.listClientes[i].cpf.ToString(),
+                                            consultarClientes.listClientes[i].emailCliente.ToString());
+                }
+                if (dados.mensagens != null)
+                {
+                    MessageBox.Show("Contate o suporte \r\n" + dados.mensagens, "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+
             }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void frmConsultarClientes_Load(object sender, EventArgs e)
+        {
+           // AutoCompletar();
         }
     }
 }
